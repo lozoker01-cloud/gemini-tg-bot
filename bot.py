1bdf61bb713ed5e95ebe092a930c6475c1e39038
lozoker01-cloud <lozoker01@gmail.com>
2026-09-18 10:46:44 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
 genai.configure(api_key=GEMINI_API_KEY)
-model = genai.GenerativeModel('gemini-1.5-flash')
+model = genai.GenerativeModel('gemini-2.0-flash')
 
 bot = Bot(token=TELEGRAM_TOKEN)
 dp = Dispatcher()
